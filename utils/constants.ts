6a6eddb1d78213cdd96bcce77e28c4f3f88d7382
Serkan Reis <serkanreis@gmail.com>
2022-09-23 20:36:34 +0300

    Fetch My Collections from the API

--- utils/constants.ts
+++ utils/constants.ts
@@ -4,6 +4,7 @@ export const WHITELIST_CODE_ID = parseInt(process.env.NEXT_PUBLIC_WHITELIST_CODE
 
 export const PINATA_ENDPOINT_URL = process.env.NEXT_PUBLIC_PINATA_ENDPOINT_URL
 export const NETWORK = process.env.NEXT_PUBLIC_NETWORK
+export const API_URL = process.env.NEXT_PUBLIC_API_URL
 export const STARGAZE_URL = process.env.NEXT_PUBLIC_STARGAZE_WEBSITE_URL
 
 export const BLOCK_EXPLORER_URL = process.env.NEXT_PUBLIC_BLOCK_EXPLORER_URL
