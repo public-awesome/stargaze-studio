56e7986fbd430a5bd6535b58f16fa19b6a6ff62b
Serkan Reis <serkanreis@gmail.com>
2023-02-04 11:28:41 +0300

    Metadata file count check prior to batch minting for 1/1 collections

--- pages/collections/create.tsx
+++ pages/collections/create.tsx
@@ -676,9 +676,14 @@ const CollectionCreationPage: NextPage = () => {
     if (!uploadDetails) {
       throw new Error('Please select assets and metadata')
     }
-    // if (minterType === 'base' && uploadDetails.uploadMethod === 'new' && uploadDetails.assetFiles.length > 1) {
-    //   throw new Error('Base Minter can only mint one asset at a time. Please select only one asset.')
-    // }
+    if (
+      minterType === 'base' &&
+      uploadDetails.uploadMethod === 'new' &&
+      uploadDetails.assetFiles.length > 1 &&
+      uploadDetails.metadataFiles.length === 0
+    ) {
+      throw new Error('Please select metadata files')
+    }
     if (uploadDetails.uploadMethod === 'new' && uploadDetails.assetFiles.length === 0) {
       throw new Error('Please select the assets')
     }
