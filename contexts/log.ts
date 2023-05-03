5041d34964ac6b589a6ca9f7a8e2359a764d46a2
Serkan Reis <serkanreis@gmail.com>
2023-05-03 21:16:51 +0300

    Add log state related functions

---
+++ contexts/log.ts
@@ -0,0 +1,27 @@
+import create from 'zustand'
+
+export interface LogItem {
+  id: string
+  message: string
+  type?: string
+  timestamp?: Date
+  code?: number
+  source?: string
+  connectedWallet?: string
+}
+
+export const useLogStore = create(() => ({
+  itemList: [] as LogItem[],
+}))
+
+export const setLogItemList = (list: LogItem[]) => {
+  useLogStore.setState({ itemList: list })
+}
+
+export const addLogItem = (item: LogItem) => {
+  useLogStore.setState((prev) => ({ itemList: [...prev.itemList, item] }))
+}
+
+export const removeLogItem = (id: string) => {
+  useLogStore.setState((prev) => ({ itemList: prev.itemList.filter((item) => item.id !== id) }))
+}
