3772dec6e163a705d8df6e4f07ae26f76b892a31
Serkan Reis <serkanreis@gmail.com>
2023-08-31 23:37:29 +0300

    Init globalSettings

---
+++ contexts/globalSettings.ts
@@ -0,0 +1,11 @@
+import create from 'zustand'
+
+export type Timezone = 'UTC' | 'Local'
+
+export const useGlobalSettings = create(() => ({
+  timezone: 'UTC' as Timezone,
+}))
+
+export const setTimezone = (timezone: Timezone) => {
+  useGlobalSettings.setState({ timezone })
+}
