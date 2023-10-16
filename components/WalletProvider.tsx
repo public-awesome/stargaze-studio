17751c5455079552ee3ba3d5e7c024e15f1e1fa8
Serkan Reis <serkanreis@gmail.com>
2023-10-16 13:12:04 +0300

    Add Leap Wallet support to Wallet Provider

--- components/WalletProvider.tsx
+++ components/WalletProvider.tsx
@@ -3,6 +3,7 @@ import '@interchain-ui/react/styles'
 
 import { GasPrice } from '@cosmjs/stargate'
 import { wallets as keplrExtensionWallets } from '@cosmos-kit/keplr-extension'
+import { wallets as leapExtensionWallets } from '@cosmos-kit/leap-extension'
 import { ChainProvider } from '@cosmos-kit/react'
 import { assets, chains } from 'chain-registry'
 import { getConfig } from 'config'
@@ -23,7 +24,7 @@ export const WalletProvider = ({ children }: { children: ReactNode }) => {
           gasPrice: GasPrice.fromString(`${gasPrice}${feeToken}`),
         }),
       }}
-      wallets={keplrExtensionWallets}
+      wallets={[...keplrExtensionWallets, ...leapExtensionWallets]}
     >
       {children}
     </ChainProvider>
