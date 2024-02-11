35a9c0eba8e198627d584810708606f4f2d1f457
Serkan Reis <serkanreis@gmail.com>
2024-02-11 19:35:27 +0300

    Set up env variable for default API key

--- env.d.ts
+++ env.d.ts
@@ -104,6 +104,7 @@ declare namespace NodeJS {
     readonly NEXT_PUBLIC_STARGAZE_WEBSITE_URL: string
     readonly NEXT_PUBLIC_WEBSITE_URL: string
     readonly NEXT_PUBLIC_SYNC_COLLECTIONS_API_URL: string
+    readonly NEXT_PUBLIC_NFT_STORAGE_DEFAULT_API_KEY: string
 
     readonly NEXT_PUBLIC_MEILISEARCH_HOST: string
     readonly NEXT_PUBLIC_MEILISEARCH_API_KEY: string
