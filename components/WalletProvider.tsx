e319a4306cdd3027549d7083ca4f367121e198b9
Serkan Reis <serkanreis@gmail.com>
2024-05-05 11:41:01 +0300

    Update config for fang-1 testnet

--- components/WalletProvider.tsx
+++ components/WalletProvider.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable eslint-comments/disable-enable-pair */
+
 // Styles required for @cosmos-kit/react modal
 import '@interchain-ui/react/styles'
 
@@ -7,6 +9,7 @@ import { wallets as leapExtensionWallets } from '@cosmos-kit/leap-extension'
 import { ChainProvider } from '@cosmos-kit/react'
 import { assets, chains } from 'chain-registry'
 import { getConfig } from 'config'
+import { fangAssetList, fangTestnet } from 'config/fangTestnet'
 import type { ReactNode } from 'react'
 import { NETWORK } from 'utils/constants'
 
@@ -14,8 +17,8 @@ export const WalletProvider = ({ children }: { children: ReactNode }) => {
   const { gasPrice, feeToken } = getConfig(NETWORK)
   return (
     <ChainProvider
-      assetLists={assets}
-      chains={chains}
+      assetLists={[...assets, fangAssetList]}
+      chains={[...chains, fangTestnet]}
       endpointOptions={{
         endpoints: {
           stargaze: {
@@ -25,6 +28,10 @@ export const WalletProvider = ({ children }: { children: ReactNode }) => {
           stargazetestnet: {
             rpc: ['https://rpc.elgafar-1.stargaze-apis.com/'],
             rest: ['https://rest.elgafar-1.stargaze-apis.com/'],
+          },
+          stargazefangtestnet: {
+            rpc: ['https://rpc.fang-1.stargaze-apis.com/'],
+            rest: ['https://rest.fang-1.stargaze-apis.com/'],
           },
         },
         isLazy: true,
@@ -37,7 +44,7 @@ export const WalletProvider = ({ children }: { children: ReactNode }) => {
           gasPrice: GasPrice.fromString(`${gasPrice}${feeToken}`),
         }),
         signingStargate: () => ({
-          gasPrice: GasPrice.fromString(`${gasPrice}${feeToken}`),
+          gasPrice: GasPrice.fromString(`${0.01}${feeToken}`),
         }),
       }}
       wallets={[...keplrExtensionWallets, ...leapExtensionWallets]}
