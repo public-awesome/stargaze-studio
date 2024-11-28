f459e5bbcf704b47241c90648ce948fd130f9aa2
Serkan Reis <serkan@stargaze.dev>
2024-11-28 16:02:39 +0300

    Add Fleek as an upload service option

---
+++ services/upload/fleek.ts
@@ -0,0 +1,20 @@
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { ApplicationAccessTokenService, FleekSdk } from '@fleek-platform/sdk'
+
+export const uploadToFleek = async (fileArray: File[], clientId: string, directoryName: string): Promise<string> => {
+  const applicationService = new ApplicationAccessTokenService({
+    clientId,
+  })
+
+  const fleekSdk = new FleekSdk({
+    accessTokenService: applicationService,
+  })
+
+  console.log('Uploading to Fleek...')
+  const directoryCid = await fleekSdk.storage().uploadVirtualDirectory({ files: fileArray, directoryName })
+  console.log('CID: ', directoryCid.pin.cid.toString().trim())
+  return directoryCid.pin.cid.toString().trim()
+}
--- services/upload/index.ts
+++ services/upload/index.ts
@@ -1,8 +1,9 @@
+import { uploadToFleek } from './fleek'
 import type { UploadFileType } from './pinata'
 import { uploadToPinata } from './pinata'
 import { uploadToWeb3Storage } from './web3-storage'
 
-export type UploadServiceType = 'pinata' | 'web3-storage'
+export type UploadServiceType = 'pinata' | 'web3-storage' | 'fleek'
 
 export const upload = async (
   fileArray: File[],
@@ -12,7 +13,11 @@ export const upload = async (
   pinataSecretKey: string,
   web3StorageEmail: string,
   web3StorageSpaceName: string,
+  fleekClientId: string,
+  fleekDirectoryName: string,
 ): Promise<string> => {
   if (uploadService === 'web3-storage') return uploadToWeb3Storage(fileArray, web3StorageEmail, web3StorageSpaceName)
-  return uploadToPinata(fileArray, pinataApiKey, pinataSecretKey, fileType)
+  else if (uploadService === 'pinata') return uploadToPinata(fileArray, pinataApiKey, pinataSecretKey, fileType)
+  else if (uploadService === 'fleek') return uploadToFleek(fileArray, fleekClientId, fleekDirectoryName)
+  throw new Error('Invalid upload service')
 }
