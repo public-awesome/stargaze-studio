c30e718ce82bc0c7a5d26f40cda6b9ab2760cf5a
Serkan Reis <serkanreis@gmail.com>
2025-03-07 20:11:56 +0300

    Remove proxy for fetching MS holders on Osmosis

--- pages/snapshots/holders.tsx
+++ pages/snapshots/holders.tsx
@@ -39,7 +39,6 @@ const Holders: NextPage = () => {
 
   const snapshotEndpoint = `https://metabase.stargaze-apis.com/api/public/card/4cf9550e-5eb7-4fe7-bd3b-dc33229f53dc/query/json?parameters=%5B%7B%22type%22%3A%22category%22%2C%22value%22%3A%22${collectionAddressState.value}%22%2C%22id%22%3A%22cb34b7a8-70cf-ba86-8d9c-360b5b2fedd3%22%2C%22target%22%3A%5B%22variable%22%2C%5B%22template-tag%22%2C%22collection_addr%22%5D%5D%7D%5D`
   const madScientistsEndPoint = `https://www.eleiton.dev/api/madscientists/snapshot?chain=stargaze`
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
 
   const download = (content: string, fileName: string, contentType: string) => {
     const a = document.createElement('a')
@@ -175,7 +174,7 @@ const Holders: NextPage = () => {
             collectionAddressState.value.trim() === 'stars1v8avajk64z7pppeu45ce6vv8wuxmwacdff484lqvv0vnka0cwgdqdk64sf'
           ) {
             fetchPromises.push(
-              fetch(proxyUrl + madScientistsEndPoint)
+              fetch(madScientistsEndPoint)
                 .then((response) => response.json())
                 .then((data) => {
                   console.log('Successfully fetched Mad Scientists holders')
